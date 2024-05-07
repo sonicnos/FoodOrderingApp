7a143295fe14826c71dd6601b588d12c667bdc48
Loukas <loukasbass@gmail.com>
2024-05-07 18:34:46 +0200

    '07/05/2024'

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -9,6 +9,8 @@ export default function MenuScreen() {
       data={products}
       renderItem={({ item }) => <ProductListItem product={item} />}
       numColumns={2}
+      contentContainerStyle={{ gap: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
     />
   );
 }
